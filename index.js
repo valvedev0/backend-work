9a726ee203d90d45b31fc904e1af1bd7eb122cdf
Praveen <aman.m404@outlook.com>
2023-11-13 17:50:49 +0530

    modified changes

--- index.js
+++ index.js
@@ -3,20 +3,38 @@ const port = 8000;
 const fs = require('fs');
 
 function requestHandler(req, res)
-{
+{ 
     console.log(req.url);
     res.writeHead(200, {'content-type':'text/html'});
 
-   
-    fs.readFile('./index.html', function(err, data) {
-    if (err)
+
+// create  a file path to switch bw pages
+    let filePath;
+
+    switch(req.url)
     {
-        console.log('error', err)
-        return res.end('<h1> error </h1>');
+        case '/':
+            filePath = './index.html'
+            break;
+        case '/profile':
+            filePath = './profile.html'
+            break;
+        default :
+            filePath = './404.html';
+
+    }
+
+
+   
+    fs.readFile(filePath, function(err, data) {
+  if (err)
+   {
+       console.log('error', err)
+      return res.end('<h1> error </h1>');
     }
 
     return res.end(data);
-  });
+   });
 
     //res.end('<h1> ok </h1>');
 }
