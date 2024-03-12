fe41f82d5197a23f23fb39644bf30f9c1906e053
Praveen <aman.m404@outlook.com>
2024-03-12 09:39:48 +0530

    added parser

--- contact_list/index.js
+++ contact_list/index.js
@@ -6,6 +6,8 @@ const port = 8000;
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views') );
+app.use(express.urlencoded());
+
 
 var contactList = [
     {
@@ -35,7 +37,15 @@ app.get('/practice', function(req, res){
 });
 
 app.post('/create-contact', function(req, res){
-    return res.redirect('/practice');
+    // return res.redirect('/practice');
+
+    // contactList.push({
+    //     name: req.body.name,
+    //     phone: req.body.phone
+    // });
+
+    contactList.push(req.body);
+    return res.redirect('back');
 });
 
 
